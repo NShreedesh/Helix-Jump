ac5791cb359cc6b15895eeb802747776a8e7a163
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2022-06-16 10:29:29 +0545

    Changes on Level and Scripts.
    -> Made Level Design prefab for different helixes.
    -> Tag for different kind of helixes assigned.
    -> Static Tag Manager with different tag names implemented.
    -> Started check for where the ball hits and perform accordingly.

--- Assets/Scripts/Ball/BallJump.cs
+++ Assets/Scripts/Ball/BallJump.cs
@@ -1,5 +1,7 @@
 using System;
+using Static;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Ball
 {
@@ -16,13 +18,35 @@ namespace Ball
         
         private void OnCollisionEnter(Collision collision)
         {
-            if(!_canJump) return;
+            if (collision.gameObject.CompareTag(TagManager.HelixNonKill))
+            {
+                if(!_canJump) return;
             
-            rb.velocity = Vector3.zero;
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            _canJump = false;
+                rb.velocity = Vector3.zero;
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                _canJump = false;
             
-            Invoke(nameof(CheckJump), 0.2f);
+                Invoke(nameof(CheckJump), 0.2f);
+            }
+            
+            else if (collision.gameObject.CompareTag(TagManager.HelixKill))
+            {
+                print("Dead");
+                SceneManager.LoadScene(0);
+            }
+            
+            else if (collision.gameObject.CompareTag(TagManager.HelixLevelComplete))
+            {
+                print("Level Completed");
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag(TagManager.HelixPoint))
+            {
+                print("Point Scored");
+            }
         }
 
         private void CheckJump()
---
+++ Assets/Scripts/Static/TagManager.cs
@@ -0,0 +1,11 @@
+
+namespace Static
+{
+    public static class TagManager
+    {
+        public const string HelixKill = "HelixKill";
+        public const string HelixNonKill = "HelixNonKill";
+        public const string HelixPoint = "HelixPoint";
+        public const string HelixLevelComplete = "HelixLevelComplete";
+    }
+}
